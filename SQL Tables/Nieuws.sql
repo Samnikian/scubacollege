02c96979f721b869d07918f2063c841cfef6b042
Niels <Niels@192.168.0.152>
2015-05-28 13:31:52 +0200

    Added SQL for Nieuws table

---
+++ SQL Tables/Nieuws.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `nieuws` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `titel` varchar(150) NOT NULL,
+  `tekst` text NOT NULL,
+  `foto` varchar(150) NOT NULL,
+  `wie` int(5) NOT NULL,
+  `aangemaakt` varchar(10) NOT NULL,
+  `gewijzigd` varchar(10) NOT NULL,
+  `prioriteit` int(3) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `titel` (`titel`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
