13ff37d711fa8beaeb8a2c0c711a4c735e5082ef
Niels <Niels@192.168.0.185>
2015-07-06 01:37:58 +0200

    Fixed last auto include error (for now)

--- kalender.php
+++ kalender.php
@@ -1,12 +1,5 @@
 <?php
-
 require_once('header.php');
-require_once "includes/JBBCode/Parser.php";
-if (true) {
-    $obj = new DiveEvent\Display($db);
-    echo $obj->getHTMLTable();
-} else {
-    echo '<p class="melding">Er staat momenteel niets op de planning!</p>';
-}
+$obj = new DiveEvent\Display($db);
+echo $obj->getHTMLTable();
 require_once('footer.php');
-?>
