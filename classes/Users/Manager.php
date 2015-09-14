0f2190c18a1ad3cd16ff44bae9dfd8e9d6f011b2
Niels <Niels@192.168.0.184>
2015-09-14 20:27:10 +0200

    Started user registrations

--- classes/Users/Manager.php
+++ classes/Users/Manager.php
@@ -1,38 +1,71 @@
 <?php
+
 namespace Users;
+
 class Manager {
+
+    private $email, $lidnr;
+    private $error = false;
+    private $errors = array();
+
     public function newUser() {
-        if (POST) {
+        $output = '';
+        $req = filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_STRING);
+        if ($req == 'POST') {
+            $this->validateAuthorization();
             $this->getPostData();
             $this->validateData();
-            if($heeftrechten and $isingelogt) {
-                if ($ok) {
-                    $this->RegisterUser();
-                }
-            } elseif(isingelogt) {
-                //TODO niet genoeg rechten
+            if (!$this->error) {
+                $output = $this->RegisterUser();
             } else {
-                redirect('index.php');
+                $output = $this->getErrors();
             }
         } else {
-            $this->getRegistrationForm();
+            $output = $this->getRegistrationForm();
         }
+        return $output;
     }
 
     private function getRegistrationForm() {
-        
+        echo 'A$$';
     }
 
     private function getPostData() {
-        
+        $this->email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $this->lidnr = filter_input(INPUT_POST, 'lidnr', FILTER_SANITIZE_NUMBER_INT);
     }
 
     private function validateData() {
-        
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $this->error = true;
+            $this->errors[] = '<li>Geef een geldig email adres op</li>';
+        }
+        if (!filter_var($this->lidnr, FILTER_VALIDATE_INT)) {
+            $this->error = true;
+            $this->errors[] = '<li>Geef een geldig lidnr op</li>';
+        }
     }
 
     private function RegisterUser() {
         
+    }
+
+    public function getUnauthorizedMessage() {
+        return '<span class="melding">U heeft niet genoeg rechten om een gebruiker toe te voegen.</span>';
+    }
+
+    public function getErrors() {
+        $output = '<ul>';
+        for($i=0;$i<count($this->errors);$i++){
+            $output.= $this->errors[$i];
+        }
+        $output.= '</ul>';
+    }
+
+    public function validateAuthorization() {
+        if ($_SESSION['user_level'] <= 3 or ! isset($_SESSION['ingelogt']) or $_SESSION['ingelogt'] === false) {
+            redirect('index.php');
+        }
     }
 
 }
